19eaf465051db5b220345c2c98ea90281231bcdf
Ihor Kostiuk <ihor.kostiuk@knu.ua>
2024-09-14 17:44:55 +0300

    refactor: class LabirinthWidget

--- libs/packman-visual/src/packmanvis/core/widgets/labirinth.py
+++ libs/packman-visual/src/packmanvis/core/widgets/labirinth.py
@@ -1,8 +1,8 @@
+from packmanvis.core.widgets.path import PlaceholderWidget
+from packmanvis.core.widgets.wall import WallWidget
 from packmanvis.types.labirinth import Labirinth
 from packmanvis.types.wall import Wall
-from PyQt6.QtWidgets import QWidget, QGridLayout
-from packmanvis.core.widgets.wall import WallWidget
-from packmanvis.core.widgets.path import PlaceholderWidget
+from PyQt6.QtWidgets import QGridLayout, QWidget
 
 
 class LabirinthWidget(QWidget):
@@ -10,7 +10,7 @@ class LabirinthWidget(QWidget):
         super(LabirinthWidget, self).__init__(parent)
         self.labirinth = labirinth
         self._setup_layout()
-        
+
     def _setup_layout(self) -> None:
         layout = QGridLayout(self)
         layout.setVerticalSpacing(0)
@@ -18,8 +18,12 @@ class LabirinthWidget(QWidget):
         for row_index, row in enumerate(self.labirinth.map):
             for column_index, column in enumerate(row):
                 layout.addWidget(
-                    WallWidget(wall_obj=column) if isinstance(column, Wall) else PlaceholderWidget("packmanvis/assets/s_path/square-32.png"),
+                    (
+                        WallWidget(wall_obj=column)
+                        if isinstance(column, Wall)
+                        else PlaceholderWidget("path_sprite:0.png")
+                    ),
                     row_index,
-                    column_index
+                    column_index,
                 )
         self.setLayout(layout)
