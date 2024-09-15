2d0f420da28c893e7cccd4ca21e5ede87d78f79d
Ihor Kostiuk <ihor.kostiuk@knu.ua>
2024-09-15 17:19:19 +0300

    refactor: created an abstraction AnimateWidget

---
+++ libs/packman-visual/src/packmanvis/core/widgets/animate.py
@@ -0,0 +1,14 @@
+from packmanvis.types.entity import Animated
+from PyQt6.QtGui import QMovie, QShowEvent
+from PyQt6.QtWidgets import QLabel, QWidget
+
+
+class AnimatedWidget(QLabel):
+    def __init__(self, animate: Animated, parent: QWidget | None = None) -> None:
+        super(AnimatedWidget, self).__init__(parent)
+        self.setMovie(QMovie(animate.gif))
+        self.resize(32, 32)
+
+    def showEvent(self, a0: QShowEvent | None) -> None:
+        self.movie().start()
+        return super().showEvent(a0)
--- libs/packman-visual/src/packmanvis/core/widgets/labirinth.py
+++ libs/packman-visual/src/packmanvis/core/widgets/labirinth.py
@@ -1,8 +1,7 @@
-from packmanvis.core.widgets.entities import CoinWidget
+from packmanvis.core.widgets.animate import AnimatedWidget
 from packmanvis.core.widgets.path import PlaceholderWidget
 from packmanvis.core.widgets.wall import WallWidget
-from packmanvis.types.coin import Coin
-from packmanvis.types.entity import Entity
+from packmanvis.types.entity import Animated
 from packmanvis.types.labirinth import Labirinth
 from packmanvis.types.wall import Wall
 from PyQt6.QtWidgets import QGridLayout, QWidget
@@ -14,23 +13,19 @@ class LabirinthWidget(QWidget):
         self.labirinth = labirinth
         self._setup_layout()
 
-    def _create_stacked_widget(self, entities: list[Entity]) -> QWidget:
+    def _create_stacked_widget(self, entities: list[Animated]) -> QWidget:
         widget = QWidget()
+
         previous_entity = PlaceholderWidget("path_sprite:0.png", widget)
         for entity in entities:
-            if isinstance(entity, Coin):
-                entity_widget = CoinWidget(coin=entity, parent=previous_entity)
-                entity_widget.resize(32, 32)
-            else:
-                raise RuntimeError(f"Instance {entity} is ot supported!")
+            entity_widget = AnimatedWidget(entity, parent=previous_entity)
             previous_entity = entity_widget
 
         return widget
 
     def _setup_layout(self) -> None:
         layout = QGridLayout(self)
-        layout.setVerticalSpacing(0)
-        layout.setHorizontalSpacing(0)
+        layout.setSpacing(0)
         for row_index, row in enumerate(self.labirinth.map):
             for column_index, column in enumerate(row):
                 layout.addWidget(
