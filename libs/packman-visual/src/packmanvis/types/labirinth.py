2beb3299f39ab4e2cc037e3d237f61650bba646a
Ihor Kostiuk <ihor.kostiuk@knu.ua>
2024-09-15 14:32:16 +0300

    chore: created home for the ghost in the maze

--- libs/packman-visual/src/packmanvis/types/labirinth.py
+++ libs/packman-visual/src/packmanvis/types/labirinth.py
@@ -27,10 +27,21 @@ class Labirinth:
             result.append(row)
         return result
 
+    @staticmethod
+    def _put_ghost_home(maze: np.ndarray) -> np.ndarray:
+        center_y, center_x = [value // 2 for value in maze.shape[:2]]
+
+        maze[center_y - 1 : center_y + 4, center_x - 3 : center_x + 2] = 0
+        maze[center_y : center_y + 3, center_x - 2 : center_x + 1] = 1
+        maze[center_y : center_y + 2, center_x - 1 : center_x] = 0
+
+        return maze
+
     @classmethod
     def generate(cls, seed: int = 42) -> Labirinth:
         maze = generate_maze(shape=(10, 20))
-        return Labirinth(walls_layout=strave_maze(maze))
+        maze = strave_maze(maze)
+        return Labirinth(walls_layout=cls._put_ghost_home(maze))
 
     def reset(self, mobs: list[Mob]) -> None:
         pass
